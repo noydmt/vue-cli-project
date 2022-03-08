d1ea9b020bdcde6a296c13d657b1ddcc9d75e4d7
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-03-08 23:27:09 +0900

    webpackを使用してプロジェクト作成し直し

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo1.png">
-  <HelloWorld :msg="msg" />
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  data () {
-    return {
-      msg: 'Hello Vue Society'
-    }
-  },
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
